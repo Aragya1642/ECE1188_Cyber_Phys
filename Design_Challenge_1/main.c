97c63a7216ebdc3c826def905beaff44f23901e7
Aragya Goyal <agoyal1642@gmail.com>
2026-03-06 10:05:49 -0500

    Does includes into Design Challenge #1

--- Design_Challenge_1/main.c
+++ Design_Challenge_1/main.c
@@ -1,5 +1,11 @@
 #include "msp.h"
-
+#include "..\inc\bump.h"
+#include "..\inc\Reflectance.h"
+#include "..\inc\Clock.h"
+#include "..\inc\SysTickInts.h"
+#include "..\inc\CortexM.h"
+#include "..\inc\LaunchPad.h"
+#include "..\inc\FlashProgram.h"
 
 /**
  * main.c
@@ -7,4 +13,6 @@
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+
 }
