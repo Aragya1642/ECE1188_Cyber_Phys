03639ca9c2371d6352aaa02f796c705cd7f7b7fc
Aragya Goyal <agoyal1642@gmail.com>
2026-03-21 12:47:28 -0400

    Update FSM - needs work on sensor readings

--- Design_Challenge_1/main.c
+++ Design_Challenge_1/main.c
@@ -12,30 +12,34 @@
 volatile uint8_t reflectance_val;
 volatile uint8_t bump_val;
 volatile uint8_t counter;
+volatile uint8_t data_ready;
 
 // Declare state struct
 /*
  *
  */
 struct State{
-    uint32_t out;                                   // 2-bit directional output
-//    uint32_t delay_ms;                              // time to delay
-    uint16_t leftDuty;                              // Left Motor PWM
-    uint16_t rightDuty;                             // Right Motor PWM
-    const struct State *next[4];                    // Next state
+    uint32_t dir_out;                                   // 2-bit directional output
+    uint16_t left_duty_percent;                              // Left Motor PWM
+    uint16_t right_duty_percent;                             // Right Motor PWM
+    const struct State *next[5];                    // Next state
 };
 typedef const struct State State_t;
 
 // Define FSM
-//#define Center  &fsm[0]
-//#define
-//#define Left    &fsm[1]
-//#define Right   &fsm[2]
-//State_t fsm[3] = {
-//    {0x03, }
-//    {0x02, }
-//    {0x01, }
-//};
+#define FWD &fsm[0]
+#define SL  &fsm[1]
+#define SR  &fsm[2]
+#define HL  &fsm[3]
+#define HR  &fsm[4]
+
+State_t fsm[5] = {
+    {0x03, 100, 100, {SL,SL,FWD,SR,SR}}               // FWD
+    {0x03, 60, 100, {HL, SL, FWD, FWD, FWD}}          // SL
+    {0x03, 100, 60, {FWD, FWD, FWD, SR, HR}}          // SR
+    {0x01, 100, 100, {HL, SL, SL, SL, SL}}            // HL
+    {0x02, 100, 100, {SR, SR, SR, SR, HR}}            // HR
+};
 
 // Declare function prototypes
 void initialize_robot(void);
@@ -44,7 +48,12 @@ void initialize_robot(void);
 // Systick Interrupt Handler
 void SysTick_Handler(void){                         // every 1ms
     counter += 1;                                   // Increment Counter
-
+    if (count == 0){
+        Reflectance_Start();
+    } else if (count == 1){
+        reflectance_val = Reflectance_End();
+        data_ready = 1;
+    }
 }
 
 // Main function
@@ -56,12 +65,9 @@ void main(void){
 
 	while(1){
 	    heart = heart^1;                            // toggle heartbeat
-	    bump_val = Bump_Read();
-	    bump_val = Bump_Read();
-	    if(bump_val){                   // any nonzero = something pressed
-	        Motor_Forward(3000, 3000);
-	    } else {
-	        Motor_Stop();
+	    if (data_ready){
+
+	        data_ready = 0;
 	    }
 	}
 }
