d6790c0ce4565f52e6d8293a3993b9425bacab24
Aragya Goyal <agoyal1642@gmail.com>
2026-03-21 12:08:32 -0400

    Motor Initialization Included

--- Design_Challenge_1/main.c
+++ Design_Challenge_1/main.c
@@ -6,6 +6,7 @@
 #include "..\inc\CortexM.h"
 #include "..\inc\LaunchPad.h"
 #include "..\inc\FlashProgram.h"
+#include "..\inc\Motor.h"
 
 // Declare global variables
 volatile uint8_t reflectance_val;
@@ -17,21 +18,24 @@ volatile uint8_t counter;
  *
  */
 struct State{
-    uint32_t out;                                   // 2-bit output
+    uint32_t out;                                   // 2-bit directional output
 //    uint32_t delay_ms;                              // time to delay
+    uint16_t leftDuty;                              // Left Motor PWM
+    uint16_t rightDuty;                             // Right Motor PWM
     const struct State *next[4];                    // Next state
 };
 typedef const struct State State_t;
 
 // Define FSM
-#define Center  &fsm[0]
-#define Left    &fsm[1]
-#define Right   &fsm[2]
-State_t fsm[3] = {
-    {0x03, }
-    {0x02, }
-    {0x01, }
-};
+//#define Center  &fsm[0]
+//#define
+//#define Left    &fsm[1]
+//#define Right   &fsm[2]
+//State_t fsm[3] = {
+//    {0x03, }
+//    {0x02, }
+//    {0x01, }
+//};
 
 // Declare function prototypes
 void initialize_robot(void);
@@ -48,10 +52,17 @@ void main(void){
     uint32_t heart = 0;                             // heartbeat
     initialize_robot();                             // initialize the robot
 	SysTick_Init(48000, 2);                         // Interrupt @ 1000Hz
+	EnableInterrupts();                             // Enable Interrupts
 
 	while(1){
-
 	    heart = heart^1;                            // toggle heartbeat
+	    bump_val = Bump_Read();
+	    bump_val = Bump_Read();
+	    if(bump_val){                   // any nonzero = something pressed
+	        Motor_Forward(3000, 3000);
+	    } else {
+	        Motor_Stop();
+	    }
 	}
 }
 
@@ -62,4 +73,5 @@ void initialize_robot(void){
     LaunchPad_Init();                               // Initialize LED/Buttons
     Bump_Init();                                    // Initialize bump sensor
     Reflectance_Init();                             // Initialize reflectance
+    Motor_Init();                                   // Initialize motors
 }
